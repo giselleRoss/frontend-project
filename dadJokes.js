1315e1e58d53bd00725dff26123954bebfd10c43
GISELLE ROSS <GISELLEROSSTECH@GMAIL.COM>
2024-01-05 08:19:42 -0500

    Added cards to each joke and fixed some errors in the show more button

--- dadJokes.js
+++ dadJokes.js
@@ -1,56 +1,114 @@
-console.log("Hello!")
+console.log("Hello!");
 //1. User goes on my page and sees a dad picture with floating block on right side
 //2. they click on img and it tells one random joke with a giggle
-//3. User can also search for jokes. 
+//3. User can also search for jokes.
 //First one will appear and user will have option to see more jokes on the list
 //4. There will be a button that user can click to see memes instead of just jokes
+
 const $result = $("#jokeText");
-const audio = new Audio('./sounds/peter-griffen-laugh.mp3');
-const $jokesContainer = $("joke-box");
+const audio = new Audio("./sounds/peter-griffen-laugh.mp3");
+const $jokesContainer = $("#jokes-container");
+const jokesPerPage = 2;
+
 //create a click event so when dad image is clicked, a random joke appears
 $("#dadImage").on("click", (event) => {
   event.preventDefault();
   setTimeout(() => {
     audio.play();
-  }, 2000)
-  
+  }, 2000);
+
   randomJoke();
 });
+
 //create another click for the search button to return jokes with that word
 $("#searchBtn").on("click", (event) => {
   event.preventDefault();
-  console.log("button clicked!")
   const searchWord = $("#searchInput").val();
-  console.log(searchWord);
-  if(searchWord.length > 0) {
+  if (searchWord.length > 0) {
     searchJokes(searchWord);
-  setTimeout(() => {
-      audio.play();
-    }, 3000)
+  } else {
+    $("#enterSearch").text("Please enter a search term");
   }
-  else {
-    $result.text("Please enter a search term")
-  }
-  
 });
 
 //create a keyboard event listener for the input when user presses enter
 $("#searchInput").on("keypress", (event) => {
-  const searchWord = $("#searchInput").val();  
-  if(event.key === "Enter" && searchWord.length > 0){
+  const searchWord = $("#searchInput").val();
+  if (event.key === "Enter" && searchWord.length > 0) {
     event.preventDefault();
-    console.log("pushed enter")
+    console.log("pushed enter");
     searchJokes(searchWord);
+  } else {
+    $("#enterSearch").text("Please enter a search term");
   }
-  else {
-    $result.text("Please enter a search term")
-  }
-  
 });
 
+$(document).ready(() => {
+  $("#scrollDownBtn").on("click", function () {
+    // Scroll to the jokes container
+    $("html, body").animate(
+      {
+        scrollTop: $jokesContainer.offset().top,
+      },
+      300
+    ); //speed
+  });
+  $("#scrollDownBtn").hide();
+});
+
+// Function to display jokes in containers
+const displayJokes = (jokes) => {
+  $jokesContainer.empty();
+  if (jokes.length > 0) {
+    //loop through each joke and create a card
+    jokes.forEach((jokeData, index) => {
+      const $card = $("<div></div>").addClass("card");
+      //create divs for a header and body
+      const $cardHeader = $("<div></div>")
+        .addClass("card-header")
+        .text(`Joke ${index + 1}`);
+      const $cardBody = $("<div></div>").addClass("card-body");
+      //create a blockquote for the joke
+      const $blockQuote = $("<blockquote></blockquote>").addClass(
+        "blockquote mb-0"
+      );
+      //add & change the text for p element
+      const $quoteText = $("<p></p>").text(jokeData.joke);
+      //add a footer to the card to add like an author
+      const $footer = $("<footer></footer>")
+        .addClass("block-footer")
+        .text(`Someone famous`);
+
+      $blockQuote.append($quoteText, $footer);
+      $cardBody.append($blockQuote);
+      $card.append($cardHeader, $cardBody);
+      $jokesContainer.append($card);
+    });
+
+    if (jokes.length > jokesPerPage) {
+      // Show the "Show More Jokes" button if more than the predefined limit
+      $("#scrollDownBtn").show("bounce");
+    } else {
+      $("#scrollDownBtn").hide(); // Hide button if no more jokes to display
+    }
+  } else {
+    $result.text("No Jokes Found");
+    $("#scrollDownBtn").hide(); // Hide button if no jokes to display
+  }
+};
+
+//create a event listener to scroll down when clicked
+$("#scrollDownBtn").on("click", () => {
+  $("html, body").animate(
+    {
+      scrollTop: $jokesContainer.offset().top,
+    },
+    300
+  );
+});
 // $(".seeMoreBtn").on("click", (event) => {
 //   event.preventDefault();
-  
+
 // });
 // create a seperate function to return the random joke using $.ajax call
 const randomJoke = () => {
@@ -61,53 +119,45 @@ const randomJoke = () => {
     method: "GET",
     url: url,
     headers: {
-    "Accept": "text/plain"
-  },
-  success: (data) => {
-    $result.text(data);
-    console.log(data)
-  },
-  error: (err) => {
-    console.error("Error getting joke", err);
-  }
-  })
-}
+      Accept: "text/plain",
+    },
+    success: (data) => {
+      $result.text(data);
+      console.log(data);
+    },
+    error: (err) => {
+      console.error("Error getting joke", err);
+    },
+  });
+};
 // create a seperate function to search for jokes using $.ajax call
 const searchJokes = (searchTerm) => {
   const url = `https://icanhazdadjoke.com/search?term=${searchTerm}`;
   const jokesContainer = $("#jokes-container");
-  // $result.empty();
 
   $.ajax({
     method: "GET",
     url: url,
     headers: {
-    "Accept": "application/JSON"
-  },
-  success: (data) => {
-    console.log(data.results)
-    if(data.results && data.results.length > 0) {
-      jokesContainer.empty(); //clear prev content
-      data.results.forEach((joke) => {
-        const $card = $("<div></div>").addClass("joke-box");
-        const $jokeText = $("<p></p>").text(joke.joke);
-        $card.append($jokeText);
-        jokesContainer.append($card);
-        jokesContainer.removeClass("d-none");
-
-      }) 
-    }
-    else {
-      jokesContainer.text("No Jokes Found")
-    }
-  },
-  error: (err) => {
-    console.error("Error getting joke", err);
-  }
-  })
-}
-
-
+      Accept: "application/JSON",
+    },
+    success: (data) => {
+      console.log(data.results);
+      if (data.results && data.results.length > 0) {
+        displayJokes(data.results);
+        setTimeout(() => {
+          audio.play();
+        }, 3000);
+      } else {
+        $result.text("No Jokes Found");
+        $("#scrollDownBtn").hide();
+      }
+    },
+    error: (err) => {
+      console.error("Error getting joke", err);
+    },
+  });
+};
 
 // $tweetContainer = $(".tweets");
 // $authorTweets = $('.user');
